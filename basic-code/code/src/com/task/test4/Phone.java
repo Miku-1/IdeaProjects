855a6cbc53b5f5f906070ac110ccecc532c3b8a8
Miku-1 <huanyue521@outlook.com>
2019-12-06 19:56:47 +0800

    练习

--- basic-code/code/src/com/task/test4/Phone.java
+++ basic-code/code/src/com/task/test4/Phone.java
@@ -2,10 +2,80 @@ package com.task.test4;
 
 public class Phone {
     // 品牌
+    private String brand;
 
     // 价格
+    private Double price;
 
     // 颜色
+    private String color;
 
-    // 主打功能
+    // Cpu
+    private String mobileCpu;
+
+    // 运行内存(G)
+    private Integer ram;
+
+    // 储存空间
+    private Integer rom;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMobileCpu() {
+        return mobileCpu;
+    }
+
+    public void setMobileCpu(String mobileCpu) {
+        this.mobileCpu = mobileCpu;
+    }
+
+    public Integer getRam() {
+        return ram;
+    }
+
+    public void setRam(Integer ram) {
+        this.ram = ram;
+    }
+
+    public Integer getRom() {
+        return rom;
+    }
+
+    public void setRom(Integer rom) {
+        this.rom = rom;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", mobileCpu='" + mobileCpu + '\'' +
+                ", ram=" + ram +
+                ", rom=" + rom +
+                '}';
+    }
 }
